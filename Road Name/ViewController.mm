2df9baab7e8cf42c605cefb817391aabde3f79f2
Yuhan Long <yuhan@MacBook-Pro-2.local>
2015-11-30 11:00:42 -0500

    static project working

--- Road Name/ViewController.mm
+++ Road Name/ViewController.mm
@@ -9,7 +9,8 @@
 #include "opencv2/opencv.hpp"
 #import "ViewController.h"
 #include "homographyUtil.hpp"
-
+#include <cmath>
+#include <vector>
 
 using namespace cv;
 using namespace std;
@@ -30,8 +31,12 @@ using namespace std;
     
     /** read the test image */
     UIImage *inputImage = [UIImage imageNamed:@"forbes.jpg"];
-    Mat roadImage = [self cvMatFromUIImage:inputImage];
     
+    //double t = (double)getTickCount();
+
+    Mat roadImage = [self cvMatFromUIImage:inputImage];
+    double t = (double)getTickCount();
+
     /** convert the road name to a image */
     Mat textImage = cvMatFromString_cv("FORBES");
     
@@ -40,15 +45,149 @@ using namespace std;
     vector<Point2f> pts_from={Point2f(0,0),Point2f(0,textImage.rows),Point2f(textImage.cols,0),Point2f(textImage.cols,textImage.rows)};
     vector<Point2f> pts_to={Point2f(500,456),Point2f(392,522),Point2f(805,450),Point2f(840,517)};
     
+    
+    
+    
+    /** Test code for boundary estimation */
+    
+    Mat roadImageGray;
+    
+    cvtColor(roadImage, roadImageGray, CV_BGR2GRAY);
+    cv::Rect roi(roadImageGray.cols/2-200,roadImageGray.rows/2, 400, roadImageGray.rows/2);
+    Mat roadImageGaussian=roadImageGray(roi);
+    
+    vector<Vec2f> lines;
+    //Mat roadImageGray;
+
+    GaussianBlur(roadImageGaussian, roadImageGaussian, cv::Size(15,15), 5);
+    resize(roadImageGaussian,roadImageGaussian,cv::Size(),0.5,0.5);
+    Canny(roadImageGaussian, roadImageGaussian, 0, 50, 3);
+    resize(roadImageGaussian,roadImageGaussian,cv::Size(),2,2,INTER_CUBIC);
+    HoughLines(roadImageGaussian, lines, 1, CV_PI/180, 200, 0, 0 );
+    cvtColor(roadImageGaussian, roadImageGaussian, CV_GRAY2BGR);
+
+    
+    vector<float> angle_range(180/5,0);
+    vector<float> r_range(180/5,0);
+    
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+        float rho = lines[i][0], theta = lines[i][1];
+        if(abs(abs(theta)-CV_PI/2)>CV_PI/18)
+        {
+            
+            size_t ind=floor((theta)/(CV_PI/36));
+            cout<<ind<<endl;
+            angle_range[ind]=theta;
+            r_range[ind]=rho;
+        }
+    }
+    
+    
+    float left_most_line_rho=0.0f;;
+    float left_most_line_theta=0.0f;
+    float right_most_line_rho=0.0f;
+    float right_most_line_theta=0.0f;
+
+    for(size_t i=8; i<36-7;++i)
+    {
+        if(angle_range[i]!=0)
+        {
+            left_most_line_rho=r_range[i];
+            left_most_line_theta=angle_range[i];
+            break;
+        }
+    }
+    
+    
+    for(int i=36-7; i>=8;--i)
+    {
+        if(angle_range[i]!=0)
+        {
+            right_most_line_rho=r_range[i];
+            right_most_line_theta=angle_range[i];
+            break;
+        }
+    }
+    
+    
+    vector<Vec2f> lines_filtered;
+    if(right_most_line_rho==left_most_line_rho)
+        cout<<"no plane detected"<<endl;
+    else
+    {
+        lines_filtered.push_back(Vec2f(left_most_line_rho,left_most_line_theta));
+        lines_filtered.push_back(Vec2f(right_most_line_rho,right_most_line_theta));
+    }
+    
+    
+    vector<Point2f> pt_to;
+    
+    for( size_t i = 0; i < lines_filtered.size(); i++ )
+    {
+        float rho = lines_filtered[i][0], theta = lines_filtered[i][1];
+        cv::Point pt1, pt2;
+        double a = cos(theta), b = sin(theta);
+        double x0 = a*rho, y0 = b*rho;
+        pt1.x = cvRound(x0 + 3000*(-b))+roadImageGray.cols/2-200;
+        pt1.y = cvRound(y0 + 3000*(a))+roadImageGray.rows/2;
+        pt2.x = cvRound(x0 - 3000*(-b))+roadImageGray.cols/2-200;
+        pt2.y = cvRound(y0 - 3000*(a))+roadImageGray.rows/2;
+        
+        
+        
+        //calculate the pt_to
+        
+        float _y1 = 5*roadImage.rows/6;
+        float _y2 = roadImage.rows;
+        
+        
+        float _ratio = ((float)pt1.x-(float)pt2.x)/((float)pt1.y-(float)pt2.y);
+        
+        float _x1 = pt1.x-((pt1.y-_y1)*_ratio);
+        float _x2 = pt1.x-((pt1.y-_y2)*_ratio);
+        
+        
+        cout<<_x1<<" "<<_y1<<" "<<_x2<<" "<<_y2<<endl;
+        pt_to.push_back(Point2f(_x1,_y1));
+        pt_to.push_back(Point2f(_x2,_y2));
+        
+        //line( roadImage, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
+    }
+    
+    
+    
+    
+    cout<<lines_filtered.size()<<endl;
+    
+    
+    
+    
+    
+    
+    
+    
+    /*************************************/
+    
+    
+    
+    
+    
+    
+    
+    
+    
     /** Find homography */
     Mat H;
-    fitHomography(pts_from, pts_to, H);
+    fitHomography(pts_from, pt_to, H);
     cout<<H<<endl;
 
     /** Project the warped road name */
     Mat resultImage;
     projHomography(roadImage, textImage, resultImage, H);
     
+    t = ((double)getTickCount() - t)/getTickFrequency();
+    cout<<"Time: "<<t<<"s"<<endl;
     
     /** Display the image */
     imageView_ = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, self.view.frame.size.width, self.view.frame.size.height)];
