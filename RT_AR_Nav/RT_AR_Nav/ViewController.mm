884b3f3e6c752d823945b7438e3aebf1ac75410a
johnny <johnnywang@gmail.com>
2015-12-01 11:25:05 -0500

    Added video parsing frame by frame. Each parsed frame is written to log file. Passing the logged frame to Yuhan's road segmenting results in "no planes found" right now.

--- RT_AR_Nav/RT_AR_Nav/ViewController.mm
+++ RT_AR_Nav/RT_AR_Nav/ViewController.mm
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController () {
-    AVPlayer *_player;
+    AVPlayer *player_;
     UIImageView *imageView_;
 }
 
@@ -21,7 +21,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    [self playVideo];
+//    [self playVideo];
+    
+    [self loadVideo];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,14 +34,60 @@
 - (void)playVideo {
     NSString *filepath = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"mp4"];
     NSURL *fileURL = [NSURL fileURLWithPath:filepath];
-    self->_player = [AVPlayer playerWithURL:fileURL];
+    self->player_ = [AVPlayer playerWithURL:fileURL];
     
-    AVPlayerLayer *layer = [AVPlayerLayer playerLayerWithPlayer:self->_player];
-    self->_player.actionAtItemEnd = AVPlayerActionAtItemEndNone;
+    AVPlayerLayer *layer = [AVPlayerLayer playerLayerWithPlayer:self->player_];
+    self->player_.actionAtItemEnd = AVPlayerActionAtItemEndNone;
     layer.frame = CGRectMake(0, 0, 1024, 768);
     [self.view.layer addSublayer: layer];
     
-    [self->_player play];
+    [self->player_ play];
+}
+
+- (void)loadVideo {
+    NSString *filepath = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"mp4"];
+    NSURL *fileURL = [NSURL fileURLWithPath:filepath];
+    
+    AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:fileURL options:nil];
+    AVAssetImageGenerator *gen = [[AVAssetImageGenerator alloc] initWithAsset:asset];
+    gen.appliesPreferredTrackTransform = YES;
+    
+    imageView_ = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, self.view.frame.size.width, self.view.frame.size.height)];
+    [self.view addSubview:imageView_];
+    imageView_.contentMode = UIViewContentModeScaleAspectFit;
+    
+    // Display 10 frames per second
+    CMTime vid_length = asset.duration;
+    float seconds = CMTimeGetSeconds(vid_length);
+    
+    int required_frames_count = seconds * 10;
+    int64_t step = vid_length.value / required_frames_count;
+    
+    int value = 0;
+    
+    for (int i = 0; i < required_frames_count; i++) {
+        
+        AVAssetImageGenerator *image_generator = [[AVAssetImageGenerator alloc] initWithAsset:asset];
+        image_generator.requestedTimeToleranceAfter = kCMTimeZero;
+        image_generator.requestedTimeToleranceBefore = kCMTimeZero;
+        
+        CMTime time = CMTimeMake(value, vid_length.timescale);
+        
+        CGImageRef image_ref = [image_generator copyCGImageAtTime:time actualTime:NULL error:NULL];
+        UIImage *thumb = [UIImage imageWithCGImage:image_ref];
+        CGImageRelease(image_ref);
+        NSString *filename = [NSString stringWithFormat:@"frame_%d.png", i];
+        NSString *pngPath = [NSHomeDirectory() stringByAppendingPathComponent:filename];
+        
+        [UIImagePNGRepresentation(thumb) writeToFile:pngPath atomically:YES];
+        
+        imageView_.image = thumb;
+//        imageView_.image = [self processImage:thumb];
+        
+        value += step;
+        
+        NSLog(@"%d: %@", value, pngPath);
+    }
 }
 
 @end
