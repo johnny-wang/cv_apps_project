46730af153b99eba6bd7c76920e3a51e70a9b716
johnny <johnnywang@gmail.com>
2015-12-06 19:46:46 -0500

    Removed previous homography and processVideo() calls so we have a fresh slate.

--- RT_AR_Nav/RT_AR_Nav/ViewController.h
+++ RT_AR_Nav/RT_AR_Nav/ViewController.h
@@ -12,7 +12,6 @@
 #import <AVFoundation/AVFoundation.h>
 #include "opencv2/highgui/cap_ios.h"
 #include "opencv2/opencv.hpp"
-#include "homographyUtil.hpp"
 
 @interface ViewController : UIViewController<MyAVVideoCameraDelegate>
 
