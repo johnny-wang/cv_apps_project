044736e1169302dc8d696cda6a95a91c82bd7b10
pweckl <pweckl@users.noreply.github.com>
2015-12-07 17:03:55 -0500

    Update ViewController.mm
    added drawText()

--- GIS_info/GIS_info/ViewController.mm
+++ GIS_info/GIS_info/ViewController.mm
@@ -125,6 +125,23 @@ using namespace std;
 */
 }
 
+- (UIImage*) drawText:(NSString*) text 
+             inImage:(UIImage*)  image 
+             atPoint:(CGPoint)   point 
+{
+
+    UIFont *font = [UIFont boldSystemFontOfSize:12];
+    UIGraphicsBeginImageContext(image.size);
+    [image drawInRect:CGRectMake(0,0,image.size.width,image.size.height)];
+    CGRect rect = CGRectMake(point.x, point.y, image.size.width, image.size.height);
+    [[UIColor whiteColor] set];
+    [text drawInRect:CGRectIntegral(rect) withFont:font]; 
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    return newImage;
+}
+
 - (void)locationManager:(CLLocationManager *)manager
     didUpdateToLocation:(CLLocation *)newLocation
            fromLocation:(CLLocation *)oldLocation
